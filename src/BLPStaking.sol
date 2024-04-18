748f3c687589747c4617f63fbc671444178d9eb2
BlastUP Contract Dev 2 <blastupsmartcontractdev2@gmail.com>
2024-04-19 01:31:16 +0300

    Fix imports

--- src/BLPStaking.sol
+++ src/BLPStaking.sol
@@ -2,8 +2,7 @@
 
 pragma solidity ^0.8.25;
 
-import {OwnableUpgradeable} from "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
