217da7ad1b26abf050d4aca6d6747a7571e73b17
blastupdev <dev@blast.io>
2024-05-08 18:14:47 +0300

    fix BLPStaking

--- src/BLPStaking.sol
+++ src/BLPStaking.sol
@@ -21,8 +21,14 @@ contract BLPStaking is Ownable {
     mapping(address => UserState) public users;
     mapping(uint256 => uint32) lockTimeToPercent;
 
+    /// @notice Token being staked.
     IERC20Metadata public stakeToken;
+
+    /// @notice Minimal balance for stake.
     uint256 public minBalance;
+
+    /// @notice Counter of deposits - withdrawals. Contains amount of funds owned by users
+    /// which are kept in the contract.
     uint256 public totalLocked;
 
     /* ========== CONSTRUCTOR ========== */
@@ -30,6 +36,13 @@ contract BLPStaking is Ownable {
     constructor(address _stakeToken, address _owner, address _points, address _pointsOperator) Ownable(_owner) {
         stakeToken = IERC20Metadata(_stakeToken);
         IBlastPoints(_points).configurePointsOperator(_pointsOperator);
+    }
+
+    /// @notice Ensures that balance of the contract is not lower than total amount owed to
+    /// users besides rewards.
+    modifier ensureSolvency() {
+        _;
+        require(stakeToken.balanceOf(address(this)) >= totalLocked, "BlastUP: insolvency");
     }
 
     /* ========== VIEWS ========== */
@@ -48,11 +61,13 @@ contract BLPStaking is Ownable {
         minBalance = _minBalance;
     }
 
-    // percent precision 1e2
+    /// @notice Maps a given lockTime in secods to yearly APY user can get by staking tokens for that period of time.
     function setLockTimeToPercent(uint256 lockTime, uint32 percent) external onlyOwner {
         lockTimeToPercent[lockTime] = percent;
     }
 
+    /// @notice Stake given amount for given amount of time
+    /// If user already has staked amount, lock is restarted.
     function stake(uint256 amount, uint256 lockTime) external {
         UserState storage user = users[msg.sender];
         uint32 percent = lockTimeToPercent[lockTime];
@@ -76,13 +91,19 @@ contract BLPStaking is Ownable {
         emit Staked(msg.sender, amount);
     }
 
-    function withdraw() external {
+    /// @notice Withdraw entire balance.
+    /// @param force Whether to withdraw without claiming rewards. Should only be used in emergency
+    /// cases when contract does not have enough funds to pay out rewards.
+    function withdraw(bool force) public {
         UserState storage user = users[msg.sender];
 
         require(user.unlockTimestamp <= block.timestamp, "BlastUP: you must wait more to withdraw");
         require(user.balance > 0, "BlastUP: you haven't anything for withdraw");
 
-        claim();
+        if (!force) {
+            claim();
+        }
+
         uint256 balance = user.balance;
         delete users[msg.sender];
 
@@ -91,7 +112,12 @@ contract BLPStaking is Ownable {
         emit Withdrawn(msg.sender, balance);
     }
 
-    function claim() public returns (uint256 reward) {
+    function withdraw() external {
+        withdraw(false);
+    }
+
+    /// @notice Claim all accrued rewards.
+    function claim() public ensureSolvency returns (uint256 reward) {
         reward = getRewardOf(msg.sender);
         if (reward > 0) {
             UserState storage user = users[msg.sender];
@@ -101,10 +127,9 @@ contract BLPStaking is Ownable {
         }
     }
 
-    function withdrawFunds(uint256 amount) external onlyOwner {
-        require(
-            stakeToken.balanceOf(address(this)) >= totalLocked + amount, "BlastUP: amount gt allowed to be withdrawn"
-        );
+    /// @notice Function for administrators to withdraw extra amounts sent to the contract
+    /// for reward payouts.
+    function withdrawFunds(uint256 amount) external onlyOwner ensureSolvency {
         stakeToken.safeTransfer(msg.sender, amount);
     }
 
